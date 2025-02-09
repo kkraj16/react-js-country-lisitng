c77d51090562175fd318cd49c2c95be62c1c51f7
kkraj16 <kkraj16@gmail.com>
2025-02-09 14:46:36 +0530

    Integrate Fetch api

--- components/CountriesList.jsx
+++ components/CountriesList.jsx
@@ -1,26 +1,53 @@
-import React from 'react'
-import countriesData from '../countriesData'
+import React, { useEffect, useState } from 'react'
+// import countriesData from '../countriesData'
 import CountryCard from './Countrycard'
 
+
+
+
 export default function CountriesList({query}) {
-  console.log(query);
+  const [countriesData, setCountriesData] = useState([]);
+  const [isdataLoaded, setIsDataLoaded] = useState(false);
+  const [ error, setError] = useState(null);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if(!isdataLoaded){
+      fetch('https://restcountries.com/v3.1/all').
+      then(res => res.json()).
+      then(data => setCountriesData(data)).
+      catch(err => setError(err));
+      setCount(count+1);
+    }
+    setIsDataLoaded(true);
+  }, [isdataLoaded])
+
   return (
-    <div className="countries-container">
-      {countriesData
-      .filter((country) =>
-        country.name.common.toLowerCase().includes(query.toLowerCase())
-      ).map((country) => {
-        return (
-          <CountryCard
-            key={country.name.common}
-            name={country.name.common}
-            flag={country.flags.svg}
-            population={country.population}
-            region={country.region}
-            capital={country.capital?.[0]}
-          />
-        )
-      })}
-    </div>
+    <>
+      <button onClick={() => setIsDataLoaded(false)}>Fetch Data</button> {'    '}
+      <button onClick={() => setCountriesData([])}>Empty</button> {'   '}
+      <label>Count: {count}</label>
+      {countriesData && (<div className="countries-container">
+        {countriesData
+        .filter((country) =>
+          country.name.common.toLowerCase().includes(query.toLowerCase())
+        ).map((country) => {
+          return (
+            <CountryCard
+              key={country.name.common}
+              name={country.name.common}
+              flag={country.flags.svg}
+              population={country.population}
+              region={country.region}
+              capital={country.capital?.[0]}
+            />
+          )
+        })}
+      </div>)}
+      {!isdataLoaded && <p>Loading...</p>}
+      {!countriesData && <p>No data found</p>}
+      {error && <p>{error}</p>}
+
+    </>
   )
 }
